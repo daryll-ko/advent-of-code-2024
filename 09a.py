8cdbd75cb309463a036e67b855dc69d453ed1bbf
Daryll Ko <daryll.ko@gmail.com>
2024-12-15 17:40:05 +0800

    add: include Day 9A solution

---
+++ 09a.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Inp:
+    rle: list[tuple[int, int]]
+    #                ID  len
+
+
+@dataclass
+class Outp:
+    checksum: int
+
+
+def get_input() -> Inp:
+    rle = []
+
+    with open("in.txt", "r") as f:
+        line = f.readline().strip()
+        counts = map(int, list(line))
+
+        for i, count in enumerate(counts):
+            if i % 2 == 0:
+                rle.append((i // 2, count))
+            else:
+                rle.append((-1, count))
+
+    rle = [(id, count) for id, count in rle if count > 0]
+
+    return Inp(rle)
+
+
+def sum_until(n: int) -> int:
+    return n * (n + 1) // 2
+
+
+def solve(inp: Inp) -> Outp:
+    i = 0
+    new_rle = []
+
+    while i < len(inp.rle):
+        if inp.rle[-1][0] == -1:
+            inp.rle.pop()
+            continue
+
+        id, count = inp.rle[i]
+        if id == -1:
+            while count > 0:
+                if inp.rle[-1][0] == -1:
+                    inp.rle.pop()
+                    continue
+
+                l_id, l_count = inp.rle[-1]
+                if l_count <= count:
+                    new_rle.append((l_id, l_count))
+                    inp.rle.pop()
+                    count -= l_count
+                else:
+                    new_rle.append((l_id, count))
+                    inp.rle[-1] = (l_id, l_count - count)
+                    count = 0
+        else:
+            new_rle.append((id, count))
+
+        i += 1
+
+    checksum = 0
+    done = 0
+    for id, count in new_rle:
+        # id * [done, done + count)
+        checksum += id * (sum_until(done + count - 1) - sum_until(done - 1))
+        done += count
+
+    return Outp(checksum)
+
+
+def show_output(outp: Outp) -> None:
+    print(outp.checksum)
+
+
+def main():
+    show_output(solve(get_input()))
+
+
+main()
